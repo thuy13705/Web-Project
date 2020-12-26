a23e99709fae3295bec362e08f3c8d161bfb2022
AnhThu1604 <58126477+AnhThu1604@users.noreply.github.com>
2020-12-26 21:28:49 +0700

    Update database.js
    add mongodb_url

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 // config/database.js
 module.exports = {
     'url': process.env.MONGODB_URL || "mongodb+srv://web_anhthu:Anhthu2410@cluster0.gnncy.mongodb.net/<dbname>?retryWrites=true&w=majority"
+    
 };
