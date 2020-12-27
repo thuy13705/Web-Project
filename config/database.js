42bab45b63fee23b87e0a48613d23db3bd4d2537
Thuy Nguyen <nlpthuy137@gmail.com>
2020-12-27 16:22:54 +0700

    connect Database

--- config/database.js
+++ config/database.js
@@ -1,5 +1,4 @@
 // config/database.js
 module.exports = {
-    'url': process.env.MONGODB_URL || "mongodb+srv://web_anhthu:Anhthu2410@cluster0.gnncy.mongodb.net/<dbname>?retryWrites=true&w=majority"
-    
+    'url': process.env.MONGODB_URL || "mongodb+srv://web_anhthu:Anhthu2410@cluster0.gnncy.mongodb.net/Menu?retryWrites=true&w=majority"
 };
