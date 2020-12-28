329464fedd7d90acfca84ea585174ba6ab16df40
Thuy Nguyen <nlpthuy137@gmail.com>
2020-12-28 23:04:02 +0700

    fig wrapper and sign in, sign up

---
+++ config/facebook.js
@@ -0,0 +1,17 @@
+module.exports = {
+    'facebookAuth': {
+        'clientID': '760380028163953', // App ID của bản
+        'clientSecret': '47b6e915fc0c0d051b524bdf5a6b61b2', // App Secret của bạn
+        'callbackURL': 'http://localhost:3000/facebook/callback'
+    },
+    // 'twitterAuth': {
+    //     'consumerKey': 'your-consumer-key-here',
+    //     'consumerSecret': 'your-client-secret-here',
+    //     'callbackURL': 'http://localhost:8080/auth/twitter/callback'
+    // },
+    // 'googleAuth': {
+    //     'clientID': 'your-secret-clientID-here',
+    //     'clientSecret': 'your-client-secret-here',
+    //     'callbackURL': 'http://localhost:8080/auth/google/callback'
+    // }
+};
