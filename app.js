8649f7ec68f154572b301662c5c84a988856a778
Thuy Nguyen <nlpthuy137@gmail.com>
2021-01-03 10:43:09 +0700

    add course

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ const session = require('express-session');
 const passport = require('passport');
 const authRouter = require('./routes/auth');
 const adminRouter = require('./routes/admin');
+const teacherRouter = require('./routes/teacher');
 const flash = require('connect-flash');
+const multer = require('multer');
+const path = require('path');
 const MongoDBStore = require('connect-mongodb-session')(session);
 
 const app = express();
@@ -18,9 +21,31 @@ mongoose.connect(configDB.url, {
   useNewUrlParser: true, 
   useUnifiedTopology: true
 });
+var Storage= multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './public/images')
+  },
+  filename: function (req, file, cb) {
+    cb(null,file.fieldname+"_"+Date.now()+path.extname(file.originalname));
+  }
+});
 
+var video= multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './public/videos')
+  },
+  filename: function (req, filevideo, cb) {
+    cb(null,filevideo.fieldname+"_"+Date.now()+path.extname(filevideo.originalname));
+  }
+});
+
+app.use(require("./middlewares/course"));
+app.use(multer({storage: Storage}).single('image'));
+app.use(multer({storage: video}).single('video'));
+app.use('/images',express.static(path.join(__dirname, 'public')));
 app.set('views','./view');
 app.use(express.static('public'));
+app.use('/images',express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 
 
@@ -46,7 +71,7 @@ app.use(passport.session());
 
 app.use(authRouter);
 app.use(adminRouter);
-
+app.use(teacherRouter);
 // pass passport for configuration
 require('./config/passport')(passport);
 
--- models/Course.js
+++ models/Course.js
@@ -5,7 +5,10 @@ const Schema = mongoose.Schema;
 const Course = new Schema({
     name: { type: String, default:''},
     description: {type: String},
-    image:{type: String, maxlength: 255},
+    price: {type: String},
+    // image:{type:String,data:Buffer,contentType:String,}, 
+    image:{type:String,require:true},
+    video:{type:String,require:true},
     createAt:{type: Date, default: Date.now},
     updateAt:{type: Date, default: Date.now},
 });
