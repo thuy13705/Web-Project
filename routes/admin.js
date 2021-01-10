f202b7d7ee3d05a874de2ed0d11198699b60ab81
AnhThu1604 <phamthianhthu2410@gmail.com>
2021-01-10 18:19:07 +0700

    update and delete of admin

--- routes/admin.js
+++ routes/admin.js
@@ -7,6 +7,7 @@ router.get('/add-teacher', adminController.getAddTeacher);
 router.post('/add-teacher',adminController.postAddTeacher);
 router.get('/teacher-list',adminController.getTeacherList);
 router.get('/teacher-list/delete/:id', adminController.getDeleteTeacher);
+router.get('/teacher-list/update/:id', adminController.updateTeacher);
 
 router.get('/add-course',teacherController.getAddCourse);
 router.post('/add-course',teacherController.postAddCourse);
@@ -15,11 +16,20 @@ router.get('/course-list',teacherController.getCourseList);
 router.get('/student-list',adminController.getStudentList);
 router.get('/add-student', adminController.getAddStudent);
 router.post('/add-student',adminController.postAddStudent);
+router.get('/student-list/delete/:id', adminController.deleteTeacher);
+router.get('/student-list/update/:id', adminController.updateTeacher);
+
 
 router.get('/add-parent-category', adminController.getAddParentCategory);
 router.post('/add-parent-category', adminController.postAddParentCategory);
+router.get('/category-list/delete/:id', adminController.deletePatentCategory);
+router.get('/category-list/update/:id', adminController.updateParentCategory);
+
 router.get('/add-child-category', adminController.getAddChildCategory);
 router.post('/add-child-category', adminController.postAddChildCategory);
+router.get('/category-list/delete/:id', adminController.deleteChildCategory);
+router.get('/category-list/update/:id', adminController.updateChildCategory);
+
 router.get('/category-list',adminController.getCategoryList);
 
 module.exports = router;
