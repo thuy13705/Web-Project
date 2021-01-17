b7e120a710cc5d9673aad694ef81272a4b7ff3a9
AnhThu1604 <phamthianhthu2410@gmail.com>
2021-01-17 19:26:34 +0700

    update home and add validate

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,33 +1,33 @@
-const passport = require("passport");
-const Users = require("../controllers/user");
-var bcrypt = require("bcryptjs");
-var randomstring = require("randomstring");
+const passport = require('passport');
+const Users = require('../controllers/user');
+var bcrypt = require('bcryptjs');
+var randomstring = require('randomstring');
 const User = require('../models/user');
-const nodemailer=require('nodemailer')
+const nodemailer = require('nodemailer');
 
 exports.getLogin = (req, res, next) => {
-  const message = req.flash("error")[0];
+  const message = req.flash('error')[0];
   if (!req.isAuthenticated()) {
-    res.render("signin", {
-      title: "Sign In",
+    res.render('signin', {
+      title: 'Sign In',
       message: `${message}`,
       user: req.user,
     });
   } else {
-    res.redirect("/");
+    res.redirect('/');
   }
-    //   res.render("signin", {
-    //   title: "Đăng nhập",
-    //   message: `${message}`,
-    //   user: req.user,
-    // });
+  //   res.render("signin", {
+  //   title: "Đăng nhập",
+  //   message: `${message}`,
+  //   user: req.user,
+  // });
 };
 
 exports.postLogin = (req, res, next) => {
-  passport.authenticate("local-signin", {
-    successReturnToOrRedirect: "/info",
-    failureRedirect: "/signin",
-    failureFlash: true
+  passport.authenticate('local-signin', {
+    successReturnToOrRedirect: '/info',
+    failureRedirect: '/signin',
+    failureFlash: true,
   })(req, res, next);
 };
 
@@ -36,74 +36,71 @@ exports.getLogout = (req, res, next) => {
     req.session.user = null;
   }
   req.logout();
-  res.redirect("/");
+  res.redirect('/');
 };
 
 exports.getSignUp = (req, res, next) => {
-  const message = req.flash("error")[0];
+  const message = req.flash('error')[0];
   if (!req.isAuthenticated()) {
-    res.render("signup", {
-      title: "Sign Up",
+    res.render('signup', {
+      title: 'Sign Up',
       message: `${message}`,
       user: req.user,
     });
   } else {
-    res.redirect("/");
+    res.redirect('/');
   }
-    // res.render("signup", {
-    //   title: "Đăng ký",
-    //   message: `${message}`,
-    //   user: req.user,
-    // });
+  // res.render("signup", {
+  //   title: "Đăng ký",
+  //   message: `${message}`,
+  //   user: req.user,
+  // });
 };
 
 exports.postSignUp = (req, res, next) => {
-  passport.authenticate("local-signup", {
-    successReturnToOrRedirect: "/verify-email",
-    failureRedirect: "/signup",
-    failureFlash: true
+  passport.authenticate('local-signup', {
+    successReturnToOrRedirect: '/verify-email',
+    failureRedirect: '/signup',
+    failureFlash: true,
   })(req, res, next);
 };
 
-
 exports.getVerifyEmail = (req, res, next) => {
   var transporter = nodemailer.createTransport({
-    service: "Gmail",
+    service: 'Gmail',
     auth: {
-      user: "nlpthuy137@gmail.com",
-      pass: "Thuy13705#"
-    }
+      user: 'nlpthuy137@gmail.com',
+      pass: 'Thuy13705#',
+    },
   });
-  User.findOne({ username: req.user.username }).then(user => {
+  User.findOne({ username: req.user.username }).then((user) => {
     var verification_token = randomstring.generate({
-      length: 10
+      length: 10,
     });
     var mainOptions = {
-      from: "Crepp so gud",
+      from: 'Crepp so gud',
       to: req.user.email,
-      subject: "Mã kích hoạt tài khoản trên Academy",
-      text: "text ne",
-      html:
-        "<p>Cảm ơn đã đăng kí tài khoản của Academy Online. Mã kích hoạt của bạn là:</p>" +
-        verification_token
+      subject: 'Mã kích hoạt tài khoản trên Academy',
+      text: 'text ne',
+      html: '<p>Cảm ơn đã đăng kí tài khoản của Academy Online. Mã kích hoạt của bạn là:</p>' + verification_token,
     };
     transporter.sendMail(mainOptions, (err, info) => {
       if (err) {
         console.log(err);
       } else {
-        console.log("Sent:" + info.response);
+        console.log('Sent:' + info.response);
       }
     });
     user.verify_token = verification_token;
     user.save();
   });
 
-  const message = req.flash("error")[0];
-  
-  res.render("verify-email", {
-    title: "Xác thực email",
+  const message = req.flash('error')[0];
+
+  res.render('verify-email', {
+    title: 'Xác thực email',
     message: `${message}`,
-    user: req.user
+    user: req.user,
   });
 };
 
@@ -113,21 +110,21 @@ exports.postVerifyEmail = (req, res, next) => {
     if (token == user.verify_token) {
       user.isAuthenticated = true;
       user.save();
-      return res.redirect("/signin");
+      return res.redirect('/signin');
     } else if (token != user.verify_token) {
-      req.flash("error", "Mã xác thực không hợp lệ");
-      return res.redirect("/verify-email");
+      req.flash('error', 'Mã xác thực không hợp lệ');
+      return res.redirect('/verify-email');
     }
   });
 };
 
 exports.getForgotPass = (req, res, next) => {
-  const message = req.flash("error")[0];
+  const message = req.flash('error')[0];
 
-  res.render("forgot-password", {
-    title: "Quên mật khẩu",
+  res.render('forgot-password', {
+    title: 'Quên mật khẩu',
     message: `${message}`,
-    user: req.user
+    user: req.user,
   });
 };
 
@@ -135,82 +132,90 @@ exports.postForgotPass = (req, res, next) => {
   const email = req.body.email;
   User.findOne({ email: email }, (err, user) => {
     if (!user) {
-      req.flash("error", "Email không hợp lệ");
-      return res.redirect("/forgot-password");
+      req.flash('error', 'Email không hợp lệ');
+      return res.redirect('/forgot-password');
     } else {
       var transporter = nodemailer.createTransport({
-        service: "Gmail",
+        service: 'Gmail',
         auth: {
-          user: "nlpthuy137@gmail.com",
-          pass: "Thuy13705#"
-        }
+          user: 'nlpthuy137@gmail.com',
+          pass: 'Thuy13705#',
+        },
       });
       var tpass = randomstring.generate({
-        length: 6
+        length: 6,
       });
       var mainOptions = {
-        from: "Crepp so gud",
+        from: 'Crepp so gud',
         to: email,
-        subject: "Test",
-        text: "text ne",
-        html: "<p>Mật khẩu mới của bạn là:</p>" + tpass
+        subject: 'Test',
+        text: 'text ne',
+        html: '<p>Mật khẩu mới của bạn là:</p>' + tpass,
       };
       transporter.sendMail(mainOptions, (err, info) => {
         if (err) {
           console.log(err);
         } else {
-          console.log("Sent:" + info.response);
+          console.log('Sent:' + info.response);
         }
       });
-      bcrypt.hash(tpass, 12).then(hashPassword => {
+      bcrypt.hash(tpass, 12).then((hashPassword) => {
         user.password = hashPassword;
         user.save();
       });
 
-      res.redirect("/signin");
+      res.redirect('/signin');
     }
   });
 };
 
-
 exports.getChangePassword = (req, res, next) => {
-  const message = req.flash("error")[0];
-  res.render("password-change", {
-    title: "Đổi mật khẩu",
+  const message = req.flash('error')[0];
+  res.render('password-change', {
+    title: 'Đổi mật khẩu',
     message: `${message}`,
     user: req.user,
   });
 };
 
 exports.postChangePassword = (req, res, next) => {
-  bcrypt.compare(req.body.oldpass, req.user.password, function(err, result) {
-    console.log("alo?");
+  bcrypt.compare(req.body.oldpass, req.user.password, function (err, result) {
+    console.log('alo?');
     if (!result) {
-      req.flash("error", "Mật khẩu cũ không đúng!");
-      return res.redirect("back");
+      req.flash('error', 'Mật khẩu cũ không đúng!');
+      return res.redirect('back');
     } else if (req.body.newpass != req.body.newpass2) {
       console.log(req.body.newpass);
       console.log(req.body.newpass2);
-      req.flash("error", "Nhập lại mật khẩu không khớp!");
-      return res.redirect("back");
+      req.flash('error', 'Nhập lại mật khẩu không khớp!');
+      return res.redirect('back');
     } else {
-      bcrypt.hash(req.body.newpass, 12).then(hashPassword => {
+      bcrypt.hash(req.body.newpass, 12).then((hashPassword) => {
         req.user.password = hashPassword;
         req.user.save();
       });
-      req.flash("success", "Đổi mật khẩu thành công!");
-      res.redirect("/info");
+      req.flash('success', 'Đổi mật khẩu thành công!');
+      res.redirect('/info');
     }
   });
 };
 
-exports.getFacebook=(req,res,next)=>{
-  passport.authenticate('facebook', {scope: ['email']});
+exports.getFacebook = (req, res, next) => {
+  passport.authenticate('facebook', { scope: ['email'] });
 };
 
-exports.getFacebookCallback=(req,res,next)=>{
+exports.getFacebookCallback = (req, res, next) => {
   passport.authenticate('facebook', {
     successRedirect: '/info',
-    failureRedirect: '/'
-})};
+    failureRedirect: '/',
+  });
+};
 
+exports.checkEmail = (req, res) => {
+  User.findOne({ email: req.query.email }, (err, user) => {
+    if (err) {
+      res.json(false);
+    }
+    res.json(user === null);
+  });
+};
--- routes/admin.js
+++ routes/admin.js
@@ -1,24 +1,22 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
-const teacherController = require("../controllers/teacher");
-const adminController = require("../controllers/admin");
+const teacherController = require('../controllers/teacher');
+const adminController = require('../controllers/admin');
 
 router.get('/add-teacher', adminController.getAddTeacher);
-router.post('/add-teacher',adminController.postAddTeacher);
-router.get('/teacher-list',adminController.getTeacherList);
+router.post('/add-teacher', adminController.postAddTeacher);
+router.get('/teacher-list', adminController.getTeacherList);
 router.get('/teacher-list/delete/:id', adminController.getDeleteTeacher);
 router.get('/teacher-list/update/:id', adminController.getUpdateTeacher);
 router.get('/teacher-list/update/:id', adminController.getUpdateTeacher);
 
-router.get('/add-course',teacherController.getAddCourse);
-router.post('/add-course',teacherController.postAddCourse);
-router.get('/course-list',teacherController.getCourseList);
+router.get('/add-course', teacherController.getAddCourse);
+router.post('/add-course', teacherController.postAddCourse);
+router.get('/course-list', teacherController.getCourseList);
 
-router.get('/student-list',adminController.getStudentList);
+router.get('/student-list', adminController.getStudentList);
 router.get('/add-student', adminController.getAddStudent);
-router.post('/add-student',adminController.postAddStudent);
-
-
+router.post('/add-student', adminController.postAddStudent);
 
 router.get('/add-parent-category', adminController.getAddParentCategory);
 router.post('/add-parent-category', adminController.postAddParentCategory);
@@ -30,9 +28,8 @@ router.post('/add-child-category', adminController.postAddChildCategory);
 router.get('/category-list/deleteChild/:id', adminController.getDeleteChildCategory);
 router.post('/category-list/updateChild/:id', adminController.postUpdateChildCategory);
 
-router.get('/category-list',adminController.getCategoryList);
+router.get('/category-list', adminController.getCategoryList);
 router.get('/lock-user/:id', adminController.getLockUser);
 router.get('/lock-course/:id', adminController.getLockCourse);
-
 
 module.exports = router;
